2f61fed4b55a7a586d1a3bdc889bda77b8c0fe28
Venchislaw <venchislavcodes@gmail.com>
2024-06-28 13:38:23 +0300

    Yo

--- Course1/DataStructures/binary_tree.py
+++ Course1/DataStructures/binary_tree.py
@@ -20,6 +20,12 @@ class BinaryTree:
 
         return root
 
+    def min_value_node(self, root):
+        cur = root
+
+        while cur.left:
+            cur = cur.left
+        return cur
 
     def binary_search(self, val, root):
         if not root:
@@ -32,11 +38,35 @@ class BinaryTree:
         else:
             return True
 
+    def remove_node(self, val, root):
+        # In case we didn't find node
+        if not root:
+            return None
+
+        elif val < root.val:
+            root.left = self.remove_node(val, root.left)
+        elif val > root.val:
+            root.right = self.remove_node(val, root.right)
+        # In case we found
+        else:
+            # Main removal logic:
+            if not root.left:
+                return root.right
+
+            elif not root.right:
+                return root.left
+
+            else:
+                min_node = self.min_value_node(root.right)
+                root.val = min_node.val
+
+                self.remove_node(min_node.val, root.right)
+
+
 
 root = Node(4, Node(2), Node(5))
 tree = BinaryTree(root)
 tree.insert(6, root)
 
-print(tree.binary_search(6, root))  # True
-print(tree.root.right.right.val)
-
+tree.remove_node(6, root)
+print(tree.binary_search(6, root))
