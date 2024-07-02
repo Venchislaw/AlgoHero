28aad2dd20a1085fc032bcdfc9e91df6f1b4fd7a
Venchislaw <venchislavcodes@gmail.com>
2024-07-02 10:01:39 +0300

    Heap Push

---
+++ Course1/DataStructures/heap.py
@@ -0,0 +1,19 @@
+class Heap:
+    def __init__(self):
+        self.heap = [0]
+
+    def push(self, val):
+        self.heap.append(val)
+        i = len(self.heap) - 1
+
+        while self.heap[i] < self.heap[i // 2]:
+            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
+            i = i // 2
+        return self.heap
+
+
+heap = Heap()
+heap.push(1)
+heap.push(3)
+heap.push(2)
+print(heap.push(0.5))
