a00d0df8ff3643e991b57ce3e3bbaa9d4c6c7e85
Venchislaw <venchislavcodes@gmail.com>
2024-07-02 10:40:34 +0300

    Heap Pop

--- Course1/DataStructures/heap.py
+++ Course1/DataStructures/heap.py
@@ -11,9 +11,41 @@ class Heap:
             i = i // 2
         return self.heap
 
+    def pop(self):
+        # edge cases
+        if len(self.heap) == 1:
+            return None
+        elif len(self.heap) == 2:
+            return self.heap.pop()
+
+        value_popped = self.heap[1]
+
+        i = len(self.heap) - 1
+        self.heap[1] = self.heap[i]
+        i = 1
+
+        while i * 2 < len(self.heap):
+            # right value:
+            if (i * 2 + 1 < len(self.heap) and
+                    (self.heap[i * 2 + 1] < self.heap[i * 2]
+                     and self.heap[i] > self.heap[i * 2 + 1])):
+                self.heap[i], self.heap[i * 2 + 1] = self.heap[i * 2 + 1], self.heap[i]
+                i = i * 2 + 1
+            # left value:
+            elif self.heap[i] > self.heap[i * 2]:
+                self.heap[i], self.heap[i * 2] = self.heap[i * 2], self.heap[i]
+                i *= 2
+            else:
+                break
+
+        return value_popped
+
 
 heap = Heap()
 heap.push(1)
 heap.push(3)
 heap.push(2)
 print(heap.push(0.5))
+
+print(heap.pop())
+print(heap.pop())
