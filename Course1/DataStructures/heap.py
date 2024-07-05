0bcebdf08568d84c4a958a58a4da54304382f2c4
Venchislaw <venchislavcodes@gmail.com>
2024-07-05 12:07:33 +0300

    Heap recap

--- Course1/DataStructures/heap.py
+++ Course1/DataStructures/heap.py
@@ -2,50 +2,20 @@ class Heap:
     def __init__(self):
         self.heap = [0]
 
+
     def push(self, val):
         self.heap.append(val)
         i = len(self.heap) - 1
-
-        while self.heap[i] < self.heap[i // 2]:
+        while val < self.heap[i // 2]:
             self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
             i = i // 2
-        return self.heap
 
-    def pop(self):
-        # edge cases
-        if len(self.heap) == 1:
-            return None
-        elif len(self.heap) == 2:
-            return self.heap.pop()
-
-        value_popped = self.heap[1]
-
-        i = len(self.heap) - 1
-        self.heap[1] = self.heap[i]
-        i = 1
-
-        while i * 2 < len(self.heap):
-            # right value:
-            if (i * 2 + 1 < len(self.heap) and
-                    (self.heap[i * 2 + 1] < self.heap[i * 2]
-                     and self.heap[i] > self.heap[i * 2 + 1])):
-                self.heap[i], self.heap[i * 2 + 1] = self.heap[i * 2 + 1], self.heap[i]
-                i = i * 2 + 1
-            # left value:
-            elif self.heap[i] > self.heap[i * 2]:
-                self.heap[i], self.heap[i * 2] = self.heap[i * 2], self.heap[i]
-                i *= 2
-            else:
-                break
-
-        return value_popped
+        return self
 
 
 heap = Heap()
-heap.push(1)
-heap.push(3)
-heap.push(2)
-print(heap.push(0.5))
+heap = heap.push(5)
+heap = heap.push(3)
+heap = heap.push(1)
 
-print(heap.pop())
-print(heap.pop())
+print(heap.heap)
