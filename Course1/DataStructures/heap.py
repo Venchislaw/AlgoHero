f24cbbe3b440ed240f0d78f2bb7746a38e179392
Venchislaw <venchislavcodes@gmail.com>
2024-07-06 12:56:08 +0300

    Heapify method

--- Course1/DataStructures/heap.py
+++ Course1/DataStructures/heap.py
@@ -2,7 +2,6 @@ class Heap:
     def __init__(self):
         self.heap = [0]
 
-
     def push(self, val):
         self.heap.append(val)
         i = len(self.heap) - 1
@@ -12,7 +11,6 @@ class Heap:
 
         return self
 
-
     def pop(self):
         popped = self.heap[1]
         self.heap[1] = self.heap.pop()
@@ -21,8 +19,8 @@ class Heap:
 
         while i * 2 < len(self.heap):
             if (i * 2 + 1 < len(self.heap)
-                and self.heap[i * 2 + 1] < self.heap[i * 2]
-                and self.heap[i * 2 + 1] < self.heap[i]
+                    and self.heap[i * 2 + 1] < self.heap[i * 2]
+                    and self.heap[i * 2 + 1] < self.heap[i]
             ):
                 self.heap[i * 2 + 1], self.heap[i] = self.heap[i], self.heap[i * 2 + 1]
                 i = i * 2 + 1
@@ -36,6 +34,32 @@ class Heap:
         # return popped
 
 
+    def heapify(self, array):
+        array.append(array[0])
+
+        self.heap = array
+
+        # we can skip half of array
+        # as elems in it don't have any children
+        cur = (len(array) - 1) // 2
+
+        while cur > 0:
+            i = cur
+            while 2 * i < len(self.heap):
+                if (2 * i + 1 < len(self.heap) and
+                        self.heap[2 * i + 1] < self.heap[2 * i] and
+                        self.heap[i] > self.heap[2 * i + 1]):
+
+                    self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
+                    i = 2 * i + 1
+                elif self.heap[i] > self.heap[2 * i]:
+                    self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
+                    i = 2 * i
+                else:
+                    break
+            cur -= 1
+
+
 heap = Heap()
 heap = heap.push(5)
 heap = heap.push(3)
@@ -46,4 +70,8 @@ heap.pop()
 print(heap.heap)
 heap = heap.push(10)
 heap.pop()
+print(heap.heap)
+
+
+heap.heapify([1, 2, 4, 6, 5])
 print(heap.heap)
