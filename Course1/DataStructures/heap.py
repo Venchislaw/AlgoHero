9959b18417dcc7722187b85a66d73361ee6be89c
Venchislaw <venchislavcodes@gmail.com>
2024-07-08 10:19:41 +0300

    final

--- Course1/DataStructures/heap.py
+++ Course1/DataStructures/heap.py
@@ -5,7 +5,8 @@ class Heap:
     def push(self, val):
         self.heap.append(val)
         i = len(self.heap) - 1
-        while val < self.heap[i // 2]:
+
+        while self.heap[i] < self.heap[i // 2]:
             self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
             i = i // 2
 
@@ -17,61 +18,67 @@ class Heap:
 
         i = 1
 
-        while i * 2 < len(self.heap):
+        while i * 2 < len(self.heap) - 1:
             if (i * 2 + 1 < len(self.heap)
                     and self.heap[i * 2 + 1] < self.heap[i * 2]
-                    and self.heap[i * 2 + 1] < self.heap[i]
-            ):
+                    and self.heap[i * 2 + 1] < self.heap[i]):
+
                 self.heap[i * 2 + 1], self.heap[i] = self.heap[i], self.heap[i * 2 + 1]
                 i = i * 2 + 1
 
             elif self.heap[i * 2] < self.heap[i]:
                 self.heap[i], self.heap[i * 2] = self.heap[i * 2], self.heap[i]
-                i *= 2
+                i = i * 2
+
             else:
-                return popped
+                break
 
-        # return popped
-
+        return popped
 
     def heapify(self, array):
         array.append(array[0])
-
         self.heap = array
 
-        # we can skip half of array
-        # as elems in it don't have any children
-        cur = (len(array) - 1) // 2
+        cur = (len(self.heap) - 1) // 2
 
         while cur > 0:
             i = cur
-            while 2 * i < len(self.heap):
-                if (2 * i + 1 < len(self.heap) and
-                        self.heap[2 * i + 1] < self.heap[2 * i] and
-                        self.heap[i] > self.heap[2 * i + 1]):
 
-                    self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
-                    i = 2 * i + 1
-                elif self.heap[i] > self.heap[2 * i]:
-                    self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
-                    i = 2 * i
+            while i * 2 < len(self.heap) - 1:
+                if (i * 2 + 1 < len(self.heap)
+                        and self.heap[i * 2 + 1] < self.heap[i * 2]
+                        and self.heap[i * 2 + 1] < self.heap[i]):
+
+                    self.heap[i * 2 + 1], self.heap[i] = self.heap[i], self.heap[i * 2 + 1]
+                    i = i * 2 + 1
+
+                elif self.heap[i * 2] < self.heap[i]:
+                    self.heap[i], self.heap[i * 2] = self.heap[i * 2], self.heap[i]
+                    i = i * 2
+
                 else:
                     break
             cur -= 1
 
+        return self.heap
+
 
 heap = Heap()
-heap = heap.push(5)
-heap = heap.push(3)
-heap = heap.push(1)
+heap.push(16)
+heap.push(19)
+heap.push(19)
+heap.push(21)
+heap.push(26)
+heap.push(30)
+heap.push(68)
+heap.push(65)
 
 print(heap.heap)
-heap.pop()
-print(heap.heap)
-heap = heap.push(10)
-heap.pop()
+
+print("--------------")
+print(heap.pop())
+
 print(heap.heap)
 
-
-heap.heapify([1, 2, 4, 6, 5])
+heap.heapify([65, 68, 30, 16, 19, 19, 26, 21])
 print(heap.heap)
