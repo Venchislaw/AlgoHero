4a62013a7c2076de0bce185d26f5e608aaf66afb
Venchislaw <venchislavcodes@gmail.com>
2024-07-21 12:19:35 +0300

    recap

--- Course1/Sorting/bucket_sort.py
+++ Course1/Sorting/bucket_sort.py
@@ -1,16 +1,15 @@
-def bucket_sort(array):
-    buckets = [0 for _ in range(max(array) + 1)]
-    print(buckets)
-
-    for n in array:
-        buckets[n] += 1
-
-    i = 0
-    for n in range(len(buckets)):
-        for _ in range(buckets[n]):
-            array[i] = n
-            i += 1
-    return array
-
-
-print(bucket_sort([2, 5, 0, 1, 0, 1, 1]))
+def bucket_sort(arr):
+    counts = [0] * (max(arr) + 1)
+    
+    for val in arr:
+        counts[val] += 1
+    
+    k = 0
+    for i in range(len(counts)):
+        for _ in range(counts[i]):
+            arr[k] = i
+            k += 1
+            
+    return arr
+    
+print(bucket_sort([3, 2, 0, 1, 3, 4, 6]))
