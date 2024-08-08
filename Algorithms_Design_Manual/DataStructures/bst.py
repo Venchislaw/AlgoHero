a11d5bbb8d47a510aec8e94bf420aff148760089
Venchislaw <venchislavcodes@gmail.com>
2024-08-08 11:00:51 +0300

    MTV

--- Algorithms_Design_Manual/DataStructures/bst.py
+++ Algorithms_Design_Manual/DataStructures/bst.py
@@ -21,11 +21,41 @@ class BST:
         else:
             return root
 
-left = TreeNode(1)
-right = TreeNode(3)
-root = TreeNode(2, left=left, right=right)
-left.parent = root; right.parent = root
+    def insert(self, val):
+        if not self.root:
+            self.root = TreeNode(val)
+        else:
+            current = self.root
+            
+            while True:
+                if val < current.val:
+                    if not current.left:
+                        current.left = TreeNode(val)
+                        current.left.parent = current
+                        break
+                    current = current.left
+                
+                elif val > current.val:
+                    if not current.right:
+                        current.right = TreeNode(val)
+                        current.parent = current
+                        break
+                    current = current.right
+                
+                else:
+                    break
+                
+    def print_out(self, root):
+        if root:
+            self.print_out(root.left)
+            print(root.val)
+            self.print_out(root.right)
+        
 
+root = TreeNode(2)
 
 tree = BST(root)
-print(tree.search(root, left.val))
+tree.insert(1)
+tree.insert(3)
+tree.print_out(root)
+print(root.left.parent is root)  # True
