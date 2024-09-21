7140bb778f667d63d86766bebd26c22575bb974d
Venchislaw <venchislavcodes@gmail.com>
2024-09-21 16:54:33 +0300

    Rewritten in VIM + few changes
    Finally out of my apathy🎉

--- Algorithms_Sedgewick/I/Module6/bottom_up_mergesort.py
+++ Algorithms_Sedgewick/I/Module6/bottom_up_mergesort.py
@@ -1,39 +1,46 @@
 def bottom_up_merge_sort(arr):
     n = len(arr)
     size = 1
+
     while size < n:
-        for left in range(0, n, 2 * size):
-            mid = min(left + size, n)
-            right = min(left + 2 * size, n)
-            merge(arr, left, mid, right)
+        for left in range(0, n, size*2):
+            l_end = min(left + size, n)
+            r_end = min(l_end + size, n)
+
+            merge(arr, left, l_end, r_end)
         size *= 2
 
-def merge(arr, left, mid, right):
-    # NOTE: These arrays are temporary and do not mind later changes in arr.
-    left_part = arr[left:mid]
-    right_part = arr[mid:right]
+
+def merge(arr, left, l_end, r_end):
+    l_part = arr[left:l_end]
+    r_part = arr[l_end:r_end]
+
     i = j = 0
     k = left
 
-    while i < len(left_part) and j < len(right_part):
-        if left_part[i] <= right_part[j]:
-            arr[k] = left_part[i]
+    while i < len(l_part) and j < len(r_part):
+        if l_part[i] < r_part[j]:
+            arr[k] = l_part[i]
             i += 1
+
         else:
-            arr[k] = right_part[j]
+            arr[k] = r_part[j]
             j += 1
+
         k += 1
 
-    while i < len(left_part):
-        arr[k] = left_part[i]
+    # residuals
+    while i < len(l_part):
+        arr[k] = l_part[i]
         i += 1
         k += 1
 
-    while j < len(right_part):
-        arr[k] = right_part[j]
+    while j < len(r_part):
+        arr[k] = r_part[j]
         j += 1
         k += 1
 
-arr = [5, 3, 5, 4, 2, 8, 1]
+
+arr = [3, 2, 4, 1]
 bottom_up_merge_sort(arr)
-print(arr)  # [1, 2, 3, 4, 5, 5, 8]
+print(arr)
